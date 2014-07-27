802eae69f323311df0d880aa93906b231ee58856
Andrew Connell <me@andrewconnell.com>
2014-07-27 08:31:58 -0400

    first physical project with broadboard

---
+++ SimpleBroadboardButtonLed/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace SimpleBroadboardButtonLed {
+  public class Program {
+    public static void Main() {
+      // GPIO that goes to 330ohm resistor connected to long arm of the LED
+      var led = new OutputPort(Pins.GPIO_PIN_D0, false);
+
+      // GPIO that goes to the button on the breadboard
+      var button = new InputPort(Pins.GPIO_PIN_D1, false, Port.ResistorMode.PullUp);
+
+      bool buttonState = false;
+
+      while (true)
+      {
+        // when the button is pressed...
+        buttonState = !button.Read();
+        // ... turn the light on
+        led.Write(buttonState);
+
+        Debug.Print(buttonState.ToString());
+        Thread.Sleep(100);
+      }
+
+    }
+
+  }
+}
---
+++ SimpleBroadboardButtonLed/README.md
@@ -0,0 +1,5 @@
+#SimpleBroadboardButtonLed
+
+First time using breadboard... turns the LED on when the button is pressed. *From Chapter 5 of [Getting Started with Netduino](http://www.amazon.com/Getting-Started-Netduino-Chris-Walker-ebook/dp/B007AU3D2U)*
+
+![SimpleBreadboardButtonLed.png](SimpleBreadboardButtonLed.png)
