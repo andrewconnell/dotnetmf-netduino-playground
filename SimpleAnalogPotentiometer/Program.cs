7524dd99bdfa2a7d4b94a2d4a3493763d77c1fee
Andrew Connell <me@andrewconnell.com>
2014-07-28 21:44:22 -0400

    playing with analog sensors

---
+++ SimpleAnalogPotentiometer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace SimpleAnalogPotentiometer {
+  public class Program {
+    public static void Main() {
+      OutputPort led = new OutputPort(Pins.GPIO_PIN_D0, false);
+
+      AnalogInput pot = new AnalogInput(Cpu.AnalogChannel.ANALOG_0);
+
+      double potValueInVolts = 0; // goes from ~0.0V > ~3.3V
+      int potValueInAdc = 0;      // goes from ~0 > 4095 on Netduino Plus 2 b/c it has a 12bit ADC
+
+
+      while (true) {
+
+        potValueInVolts = pot.Read() * 3.3f;
+        potValueInAdc = pot.ReadRaw();
+
+        Debug.Print("Read analog value (0.0 - 3.3v):  " + potValueInVolts + "v");
+        Debug.Print("ReadRaw analog value (0 - 4095): " + potValueInAdc);
+
+        int delay = (int)potValueInVolts * 100;
+        Debug.Print("LED delay: " + delay +"ms");
+
+        led.Write(true);
+        Thread.Sleep(delay);
+        led.Write(false);
+        Thread.Sleep(delay);
+      }
+
+    }
+
+  }
+}
---
+++ SimpleAnalogPotentiometer/README.md
@@ -0,0 +1,7 @@
+#SimpleAnalogPontentiometer
+
+Demonstrates how to get the value from an analog input via a pontentiometer (aka: a rotary dial). The debug window outputs the volts that come from the pontentiometer & a data value returned in the rage of 0-4095 on the Netduino Plus 2.
+
+*From Chapter 5 of [Getting Started with Netduino](http://www.amazon.com/Getting-Started-Netduino-Chris-Walker-ebook/dp/B007AU3D2U)*
+
+![SimpleAnalogPontentiometer.png](SimpleAnalogPontentiometer.png)
